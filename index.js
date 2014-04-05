38632b59fe9b8b201ad950af89b13da9e92f3212
suisho <suisho000000@gmail.com>
2014-04-06 05:07:20 +0900

    add speedy cache mode

--- index.js
+++ index.js
@@ -5,36 +5,42 @@ var uniq = require('uniq')
 var sortSpecificity = require("sort-specificity")
 var defaults = require("defaults")
 
-
+var detectSearch = function(selector, caches, collision){
+  var cache = caches[selector]
+  var searchs
+  if(cache){
+    searchs = cache.map(function(c){
+      return collision[c]
+    })
+    searchs = flatten(uniq(searchs))
+  }
+  return searchs
+}
 
 module.exports = function(selectors, option){
   option = defaults(option, {
-    speed : false
+    useCache : true
   })
   var result = {}
   var cache = {}
   selectors = sortSpecificity(selectors)
   selectors.forEach(function(sel){
     var searchs = selectors
-    if(option.speed){
-      var caches = cache[sel]
-      if(caches){
-        searchs = uniq(flatten(caches.map(function(c){
-          return result[c]
-        })))
+    if(option.useCache){
+      var cached = detectSearch(sel, cache, result)
+      if(cached){
+        searches = cached
       }
     }
     var r = collision(searchs, sel).sort()
     result[sel] = r
+
+    // cache
     r.forEach(function(k){
       if(!cache[k]) cache[k] = [];
       cache[k].push(sel)
     })
   })
-/*  console.log(result)
-  console.log("+++++++++++")
-  console.log(map)
-*/
   return result
 }
 var collision = function(sortedSelectors, selector){
@@ -42,8 +48,6 @@ var collision = function(sortedSelectors, selector){
 
   var extracted = sortedSelectors.filter(function(s){
     if(selector === s) return false;
-    //console.log( s, selector)
-
     return (CSSselect.selectOne(s, dom, {strict : true}) !== null)
   })
   return flatten(extracted)
