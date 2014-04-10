74a5be74d6edb91f9f9f8816347ffa78dbff16de
suisho <suisho000000@gmail.com>
2014-04-11 00:19:58 +0900

    fix

--- test/collision.js
+++ test/collision.js
@@ -1,10 +1,12 @@
 var collision = require("../lib/collision")
 var assert = require("assert")
-
+var migawari = require("migawari")
 
 describe("collision", function(){
   var isCollide = function(searchs , scapegoat){
     var result = collision([searchs], scapegoat)
+    //console.log(migawari(scapegoat).toString())
+
     return (result[0] == searchs)
   }
   it("basic", function(){
@@ -12,5 +14,8 @@ describe("collision", function(){
     assert(isCollide("a", "a b"))
     assert(isCollide("b", "a b"))
     assert(isCollide("p", "a b p"))
+  })
+  it("sibling", function(){
+    assert(isCollide("b + p", "a b + p"))
   })
 })
