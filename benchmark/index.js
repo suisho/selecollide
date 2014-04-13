c88869e8898641142a482118035484fdbe79c3a8
suisho <suisho000000@gmail.com>
2014-04-13 16:06:44 +0900

    add benchmark

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,11 +1,21 @@
 var seleflict = require("../index")
 var fixture = require("./fixture/gh.js")
 
-suite("selflict", function(){
-  bench("basic 10", function(done){
-
-    seleflict(fixture.slice(0,10), function(err, result){
-      done()
+suite("use cache performacnce", function(){
+  set('iterations', 1);
+  var countPatterns = [10,20,30,100,200]
+  countPatterns.forEach(function(count){
+    var seed = fixture.slice(0,count)
+    bench("use: " + count, function(done){
+      seleflict(seed, function(err, result){
+        done()
+      })
+    })
+    bench("not use: " + count, function(done){
+      seleflict(seed, {useCache:false}, function(err, result){
+        done()
+      })
     })
   })
+
 })
